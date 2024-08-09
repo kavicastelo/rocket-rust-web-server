c22fb125236366feabe92321abcbf44e98c7fd71
kavi castelo <kavindu123450@gmail.com>
2024-08-09 21:30:37 +0530

    base and structure

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+#[macro_use] extern crate rocket;
+
+mod routes;
+
+use routes::health::health_check;
+use routes::user::get_user;
+
+#[get("/")]
+fn index() -> &'static str {
+    "Hello, World!"
+}
+
+#[launch]
+fn rocket() -> _ {
+    rocket::build()
+        .mount("/", routes![index, health_check, get_user])
 }
